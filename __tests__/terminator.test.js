de3c3f1f1d0f8fcb7c923ef5c814adad8ea21814
Yair Tavor <yairtavor@gmail.com>
2015-12-01 19:54:31 +0100

    Terminator class - asta la vista, baby

--- __tests__/terminator.test.js
+++ __tests__/terminator.test.js
@@ -1,6 +1,17 @@
 'use strict';
 
 jest.dontMock('../lib/terminator');
+jest.setMock('../lib/pipeline-steps/format-extractor', {
+    extract: () => {
+        return { responseFormat: 'json' };
+    }
+});
+jest.setMock('../lib/parsers', {
+    search: () => ({
+        mime: 'application/json',
+        serialize: jest.genMockFunction()
+    })
+});
 
 const Terminator = require('../lib/terminator');
 let terminator;
@@ -8,5 +19,16 @@ let terminator;
 describe('Terminator', () => {
     it('get content type', () => {
         terminator = new Terminator();
+        expect(terminator.getContentType()).toEqual({'Content-Type': 'application/json'});
+    });
+
+    it('can end the request with a given code', () => {
+        terminator = new Terminator(null, {
+            writeHead: jest.genMockFunction(),
+            end: jest.genMockFunction()
+        });
+
+        terminator.terminate(Terminator.codes.BAD_REQUEST, 'invalid request, dude');
+        expect(terminator.response.end).toBeCalled();
     });
 });
